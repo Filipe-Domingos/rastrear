a872011e96e6b1fcc6338d52895533fc48606cb7
ronanrf <ronanrrf@gmail.com>
2015-10-19 22:55:05 -0200

    carga nas tabelas de tipode veiculoe categoria cnh

--- db/migrate/20151019232031_create_categorias.rb
+++ db/migrate/20151019232031_create_categorias.rb
@@ -1,44 +1,43 @@
 class CreateCategorias < ActiveRecord::Migration
   def change
-    array = Array.new
     categorias = CategoriaCnh.all
     
     if categorias.empty?
       c1 = CategoriaCnh.new
       c1.descricao = "A"
-      array << c1
+      c1.save
 
       c2 = CategoriaCnh.new
       c2.descricao = "B"
-      array << c2
+      c2.save
 
       c3 = CategoriaCnh.new
       c3.descricao = "C"
-      array << c3
+      c3.save
 
       c4 = CategoriaCnh.new
       c4.descricao = "D"
-      array << c4
+      c4.save
       
       c5 = CategoriaCnh.new
       c5.descricao = "E"
-      array << c5
+      c5.save
 
       c6 = CategoriaCnh.new
       c6.descricao = "AB"
-      array << c6
+      c6.save
 
       c7 = CategoriaCnh.new
       c7.descricao = "AC"
-      array << c7
+      c7.save
 
       c8 = CategoriaCnh.new
       c8.descricao = "AD"
-      array << c8
+      c8.save
 
       c9 = CategoriaCnh.new
       c9.descricao = "AE"
-      array << c9
+      c9.save
 
       array.save_all
     end
--- db/migrate/20151020001045_create_tipo_veiculo.rb
+++ db/migrate/20151020001045_create_tipo_veiculo.rb
@@ -1,30 +1,28 @@
 class CreateTipoVeiculo < ActiveRecord::Migration
   def change
-    array = Array.new
     tipo_veiculos = TipoVeiculo.all
 
     if tipo_veiculos.empty?
       t1 = TipoVeiculo.new
       t1.descricao = "Carro"
-      array << t1
+      t1.save
 
       t2 = TipoVeiculo.new
       t2.descricao = "Moto"
-      array << t2
+      t2.save
 
       t3 = TipoVeiculo.new
       t3.descricao = "Caminhão"
-      array << t3
+      t3.save
 
       t4 = TipoVeiculo.new
       t4.descricao = "Carreta"
-      array << t4
+      t4.save
 
       t5 = TipoVeiculo.new
       t5.descricao = "Trator"
-      array << t5
-
-      array.save_all
+      t5.save
+      
     end
   end
 end
